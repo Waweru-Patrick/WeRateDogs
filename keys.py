aa912639e9ffafa6ae90d17d4853673264b15885
Patrick Gitau <pgitauwaweru@gmail.com>
2023-03-02 16:30:01 +0300

    WeRateDogs Project Done

---
+++ keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+consumer_key = '003ZE4SkNUuwKAOWGLEJWYnZe'
+consumer_secret = 'bw7jRwqaWi1usrIvNr3wn47tdI65UTHtRtbeh2CfQrJTtmtYJQ'
+access_token = '1613606231821647899-SmcduI8mqnT7F5jY1x9OnPoI8Uq8hk'
+access_secret = 'R1OgUHy3KXOnfCweFLe7vGrPmTnWMqjyYZM0ke68hjgSO'
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAHfClAEAAAAA1KruaSF3O7qh4O3sHA%2BReAMU1RI%3DvGiZZzQNVhPxgpV8PSzWerZqQ4lE7Fz92HGr10bTWXOgciL7GH"
+
